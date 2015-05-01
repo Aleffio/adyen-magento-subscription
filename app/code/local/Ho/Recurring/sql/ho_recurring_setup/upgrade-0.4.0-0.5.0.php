7b906a9a3efd0b90ab099fa978d97c6fbb886699
Maikel Koek <maikel@h-o.nl>
2015-05-01 12:27:42 +0200

    [HO-REC] +: Customer name column added to profile

--- app/code/local/Ho/Recurring/sql/ho_recurring_setup/upgrade-0.4.0-0.5.0.php
+++ app/code/local/Ho/Recurring/sql/ho_recurring_setup/upgrade-0.4.0-0.5.0.php
@@ -12,7 +12,8 @@ $installer->run("
     ALTER TABLE `{$this->getTable('ho_recurring/profile')}`
         ADD COLUMN `status` smallint(2) unsigned DEFAULT NULL AFTER `entity_id`,
         ADD COLUMN `created_at` timestamp AFTER `store_id`,
-        ADD COLUMN `ends_at` timestamp AFTER `created_at`;
+        ADD COLUMN `ends_at` timestamp AFTER `created_at`,
+        ADD COLUMN `customer_name` varchar(255) AFTER `customer_id`;
 
 ");
 
