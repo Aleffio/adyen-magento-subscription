e491b45a455db275f2c1929c28a562ce6aedca7a
Maikel Koek <maikel@h-o.nl>
2015-05-01 13:18:42 +0200

    [HO-REC] B: Changed status column from int to varchar

--- app/code/local/Ho/Recurring/sql/ho_recurring_setup/upgrade-0.4.0-0.5.0.php
+++ app/code/local/Ho/Recurring/sql/ho_recurring_setup/upgrade-0.4.0-0.5.0.php
@@ -10,7 +10,7 @@ $installer->run("
     -- Add columns to profile table
 
     ALTER TABLE `{$this->getTable('ho_recurring/profile')}`
-        ADD COLUMN `status` smallint(2) unsigned DEFAULT NULL AFTER `entity_id`,
+        ADD COLUMN `status` varchar(255) AFTER `entity_id`,
         ADD COLUMN `created_at` timestamp AFTER `store_id`,
         ADD COLUMN `ends_at` timestamp AFTER `created_at`,
         ADD COLUMN `customer_name` varchar(255) AFTER `customer_id`;
