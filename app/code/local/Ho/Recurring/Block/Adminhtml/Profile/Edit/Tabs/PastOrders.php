7afe40315f51825f3ede5425909fcb86d03209f3
Maikel Koek <maikel@h-o.nl>
2015-05-04 12:55:18 +0200

    [HO-REC] +: Empty tabs added for past and upcoming orders

---
+++ app/code/local/Ho/Recurring/Block/Adminhtml/Profile/Edit/Tabs/PastOrders.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Ho_Recurring
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the H&O Commercial License
+ * that is bundled with this package in the file LICENSE_HO.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.h-o.nl/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to info@h-o.com so we can send you a copy immediately.
+ *
+ * @category    Ho
+ * @package     Ho_Recurring
+ * @copyright   Copyright © 2015 H&O (http://www.h-o.nl/)
+ * @license     H&O Commercial License (http://www.h-o.nl/license)
+ * @author      Maikel Koek – H&O <info@h-o.nl>
+ */
+
+class Ho_Recurring_Block_Adminhtml_Profile_Edit_Tabs_PastOrders extends Mage_Adminhtml_Block_Widget_Grid
+    implements Mage_Adminhtml_Block_Widget_Tab_Interface
+{
+    /**
+     * @return string
+     */
+    public function getTabLabel()
+    {
+        return Mage::helper('ho_recurring')->__('Past Orders');
+    }
+
+    /**
+     * @return string
+     */
+    public function getTabTitle()
+    {
+        return Mage::helper('ho_recurring')->__('Past Orders');
+    }
+
+    /**
+     * @return bool
+     */
+    public function canShowTab()
+    {
+        return true;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isHidden()
+    {
+        return false;
+    }
+}
