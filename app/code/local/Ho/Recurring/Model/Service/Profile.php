638d94ed2ef21a012724b1eb98ec7eb93d0d3007
Paul Hachmang <paul@h-o.nl>
2015-05-17 19:09:38 +0200

    Feat: Better quote creation service, fixes multiple issues, qty's prices, etc.

--- app/code/local/Ho/Recurring/Model/Service/Profile.php
+++ app/code/local/Ho/Recurring/Model/Service/Profile.php
@@ -27,64 +27,85 @@ class Ho_Recurring_Model_Service_Profile extends Mage_Core_Model_Abstract
      */
     public function createQuote(Ho_Recurring_Model_Profile $profile)
     {
-        $customerId = $profile->getCustomerId();
-        $storeId = $profile->getStoreId();
+        try {
+            if (! $profile->canCreateQuote()) {
+                Ho_Recurring_Exception::throwException('Can not create quote from profile');
+            }
 
-        $customer = Mage::getModel('customer/customer')->load($customerId);
-        $quote = Mage::getModel('sales/quote')->assignCustomer($customer);
-        $quote->setStoreId($storeId);
-        $quote->setIsActive(false);
+            if ($quote = $profile->getQuote()) {
+                Ho_Recurring_Exception::throwException('There is already an active quote present for this profile');
+            }
 
-        // Add order items to quote
-        foreach ($profile->getItems() as $item) {
-            /** @var Ho_Recurring_Model_Profile_Item $item */
-            $productId = $item->getProductId();
-            $product = Mage::getModel('catalog/product')->load($productId);
+            $storeId = $profile->getStoreId();
+            $customer = $profile->getCustomer();
+            $quote = Mage::getModel('sales/quote')->assignCustomer($customer);
+            $quote->setStoreId($storeId);
+            $quote->setIsSuperMode(true);
+            $quote->setIsActive(false); //always create an inactive quote, else it shows up on the frontend.
 
-            $quoteItem = Mage::getModel('sales/quote_item')->setProduct($product);
-            $quoteItem->setQuote($quote);
-            $quoteItem->setQty('1');
-            $quoteItem->setStoreId($storeId);
-            $quote->addItem($quoteItem);
+            // Add order items to quote
+            foreach ($profile->getItems() as $profileItem) {
+                /** @var Ho_Recurring_Model_Profile_Item $profileItem */
+                $productId = $profileItem->getProductId();
+                $product = Mage::getModel('catalog/product')->load($productId);
+
+                $quoteItem = $quote->addProduct($product, $profileItem->getQty());
+
+                $quoteItem->setCustomPrice($profileItem->getPrice());
+                $quoteItem->setOriginalCustomPrice($profileItem->getPrice());
+                $quoteItem->getProduct()->setIsSuperMode(true);
+
+                $quoteItem->setCustomPrice($profileItem->getPriceInclTax());
+                $quoteItem->setOriginalCustomPrice($profileItem->getPriceInclTax());
+                $quoteItem->setNoDiscount(false);
+                $quoteItem->getProduct()->setIsSuperMode(true);
+                $quoteItem->checkData();
+            }
+
+
+            // Set billing address data
+            /** @var Mage_Sales_Model_Quote_Address $billingAddress */
+            $quote->getBillingAddress()
+                ->addData($profile->getBillingAddressData())
+                ->setData('email', $customer->getEmail());
+
+            // Set shipping address data
+            /** @var Mage_Sales_Model_Quote_Address $shippingAddress */
+            $quote->getShippingAddress()
+                ->addData($profile->getShippingAddressData())
+                ->setData('email', $customer->getEmail())
+                ->setStockId($profile->getStockId())
+                ->setCollectShippingRates(true)
+                ->collectShippingRates();
+
+            $quote->getShippingAddress()->collectTotals();
+
+            // Set shipping method
+            $shippingMethod = $profile->getShippingMethod();
+            $quote->getShippingAddress()->setShippingMethod($shippingMethod)->save();
+
+            // Set payment method
+            $quote->getPayment()->importData(array('method' => $profile->getBillingAgreement()->getMethodCode()));
+            $methodInstance = $quote->getPayment()->getMethodInstance();
+
+            if (! method_exists($methodInstance, 'initBillingAgreementPaymentInfo')) {
+                Ho_Recurring_Exception::throwException(
+                    Mage::helper('ho_recurring')->__('Payment method %s does not support Ho_Recurring', $methodInstance->getCode()));
+            }
+
+            // Set billing agreement data
+            $methodInstance->initBillingAgreementPaymentInfo($profile->getBillingAgreement(), $quote->getPayment());
+
+            $quote->collectTotals();
+            $quote->save();
+
+            $profile->saveQuoteAtProfile($quote);
+            return $quote;
+
+        } catch (Exception $e) {
+            $profile->setStatus($profile::STATUS_QUOTE_ERROR);
+            $profile->setErrorMessage($e->getMessage());
+            throw $e;
         }
-
-        // Set billing address data
-        /** @var Mage_Sales_Model_Quote_Address $billingAddress */
-        $quote->getBillingAddress()
-            ->addData($profile->getBillingAddressData())
-            ->setData('email', $customer->getEmail());
-
-        // Set shipping address data
-        /** @var Mage_Sales_Model_Quote_Address $shippingAddress */
-        $quote->getShippingAddress()
-            ->addData($profile->getShippingAddressData())
-            ->setData('email', $customer->getEmail())
-            ->setStockId($profile->getStockId())
-            ->setCollectShippingRates(true)
-            ->collectShippingRates();
-
-        $quote->getShippingAddress()->collectTotals();
-
-        // Set shipping method
-        $shippingMethod = $profile->getShippingMethod();
-        $quote->getShippingAddress()->setShippingMethod($shippingMethod)->save();
-
-        // Set payment method
-        $paymentMethod = $profile->getPaymentMethod();
-        $quote->getPayment()->importData(array('method' => $profile->getBillingAgreement()->getMethodCode()));
-        $methodInstance = $quote->getPayment()->getMethodInstance();
-
-        if (! method_exists($methodInstance, 'initBillingAgreementPaymentInfo')) {
-            Ho_Recurring_Exception::throwException(
-                Mage::helper('ho_recurring')->__('Payment method %s does not support Ho_Recurring', $methodInstance->getCode()));
-        }
-
-        // Set billing agreement data
-        $methodInstance->initBillingAgreementPaymentInfo($profile->getBillingAgreement(), $quote->getPayment());
-
-        $quote->collectTotals();
-        $quote->save();
-
-        return $quote;
     }
 }
