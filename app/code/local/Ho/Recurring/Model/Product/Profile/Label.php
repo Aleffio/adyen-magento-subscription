f9ed4b1bab22a946bb3ab75cd838cb4a945e8c68
Maikel Koek <maikel@h-o.nl>
2015-06-03 17:14:04 +0200

    [FFM-85] +: Product profile label model and resource models

---
+++ app/code/local/Ho/Recurring/Model/Product/Profile/Label.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Ho_Recurring
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the H&O Commercial License
+ * that is bundled with this package in the file LICENSE_HO.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.h-o.nl/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to info@h-o.com so we can send you a copy immediately.
+ *
+ * @category    Ho
+ * @package     Ho_Recurring
+ * @copyright   Copyright © 2015 H&O (http://www.h-o.nl/)
+ * @license     H&O Commercial License (http://www.h-o.nl/license)
+ * @author      Maikel Koek – H&O <info@h-o.nl>
+ */
+
+/**
+ * Class Ho_Recurring_Model_Product_Profile_Label
+ *
+ * @method int getProfileId()
+ * @method $this setProfileId(int $value)
+ * @method int getStoreId()
+ * @method $this setStoreId(int $value)
+ * @method string getLabel()
+ * @method $this setLabel(string $value)
+ */
+class Ho_Recurring_Model_Product_Profile_Label extends Mage_Core_Model_Abstract
+{
+    protected function _construct ()
+    {
+        $this->_init('ho_recurring/product_profile_label');
+    }
+}
