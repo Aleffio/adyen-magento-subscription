498ea4d954fa34cdbdcd634048f25cf2a43206f5
Maikel Koek <maikel@h-o.nl>
2015-06-16 17:44:21 +0200

    [FFM-38] +: Save recurring profile in buy request of configurable product

---
+++ app/code/local/Ho/Recurring/Model/Catalog/Product/Type/Configurable.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Ho_Recurring
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the H&O Commercial License
+ * that is bundled with this package in the file LICENSE_HO.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.h-o.nl/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to info@h-o.com so we can send you a copy immediately.
+ *
+ * @category    Ho
+ * @package     Ho_Recurring
+ * @copyright   Copyright © 2015 H&O (http://www.h-o.nl/)
+ * @license     H&O Commercial License (http://www.h-o.nl/license)
+ * @author      Maikel Koek – H&O <info@h-o.nl>
+ */
+
+class Ho_Recurring_Model_Catalog_Product_Type_Configurable extends Mage_Catalog_Model_Product_Type_Configurable
+{
+    /**
+     * Prepare selected options for simple profile product
+     *
+     * @param  Mage_Catalog_Model_Product $product
+     * @param  Varien_Object $buyRequest
+     * @return array
+     */
+    public function processBuyRequest($product, $buyRequest)
+    {
+        $options = parent::processBuyRequest($product, $buyRequest);
+
+        $option = $buyRequest->getData('ho_recurring_profile');
+
+        $options['ho_recurring_profile'] = $option;
+
+        return $options;
+    }
+}
