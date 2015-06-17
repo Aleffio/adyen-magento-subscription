272310f0480426e8a9c16a1fcdc7365dd8dc2849
Maikel Koek <maikel@h-o.nl>
2015-06-17 14:29:34 +0200

    [FFM-38] +: Show Recurring Profile tab on bundle product edit page

---
+++ app/code/local/Ho/Recurring/sql/ho_recurring_setup/upgrade-0.32.0-0.33.0.php
@@ -0,0 +1,19 @@
+<?php
+
+/* @var $installer Mage_Catalog_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+/** @var Magento_Db_Adapter_Pdo_Mysql $connection */
+$connection = $installer->getConnection();
+
+$installer->updateAttribute(
+    Mage_Catalog_Model_Product::ENTITY,
+    'ho_recurring_type',
+    array(
+        'apply_to' => 'simple,configurable,bundle',
+    )
+);
+
+$installer->endSetup();
