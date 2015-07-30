73595f53c9444f63c9273fec4ab2a44f2ef4a6f5
Maikel Koek <maikel@h-o.nl>
2015-07-30 18:30:25 +0200

    [FFM-22] B: Define correct subscription_id in subscription queries
    Since the column in the order table in which the subscription is saved
    is changed to the same name, the subscription_id column can be ambiguous.

--- app/code/community/Adyen/Subscription/Model/Subscription.php
+++ app/code/community/Adyen/Subscription/Model/Subscription.php
@@ -91,7 +91,7 @@ class Adyen_Subscription_Model_Subscription extends Mage_Core_Model_Abstract
     {
         if (!$this->hasData('_active_quote_additional')) {
             $quoteAdd = Mage::getResourceModel('adyen_subscription/subscription_quote_collection')
-                ->addFieldToFilter('subscription_id', $this->getId())
+                ->addFieldToFilter('main_table.subscription_id', $this->getId())
                 ->addFieldToFilter('order_id', ['null' => true])
                 ->getFirstItem();
             $this->setData('_active_quote_additional', $quoteAdd);
@@ -145,7 +145,7 @@ class Adyen_Subscription_Model_Subscription extends Mage_Core_Model_Abstract
     public function getQuoteAdditionalCollection()
     {
         return Mage::getResourceModel('adyen_subscription/subscription_quote_collection')
-            ->addFieldToFilter('subscription_id', $this->getId());
+            ->addFieldToFilter('main_table.subscription_id', $this->getId());
     }
 
 
@@ -159,7 +159,7 @@ class Adyen_Subscription_Model_Subscription extends Mage_Core_Model_Abstract
     {
         $orderAdditional = Mage::getModel('adyen_subscription/subscription_order')
             ->getCollection()
-            ->addFieldToFilter('subscription_id', $this->getId())
+            ->addFieldToFilter('main_table.subscription_id', $this->getId())
             ->addFieldToFilter('order_id', $order->getId())
             ->getFirstItem();
 
@@ -195,7 +195,7 @@ class Adyen_Subscription_Model_Subscription extends Mage_Core_Model_Abstract
     public function getItemCollection($active = true)
     {
         $items = Mage::getResourceModel('adyen_subscription/subscription_item_collection')
-            ->addFieldToFilter('subscription_id', $this->getId());
+            ->addFieldToFilter('main_table.subscription_id', $this->getId());
 
         if ($active) {
             $items->addFieldToFilter('status', Adyen_Subscription_Model_Subscription_Item::STATUS_ACTIVE);
