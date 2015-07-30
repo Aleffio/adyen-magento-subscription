f592f7a47d4e114e33e11371c165dde3c6d15b64
rikterbeek <rikterbeek@me.com>
2015-07-30 14:17:50 +0200

    update subscriptionId if recurringReference has changes. This is the case for example Sofort

--- app/code/community/Adyen/Subscription/Model/Observer.php
+++ app/code/community/Adyen/Subscription/Model/Observer.php
@@ -394,4 +394,86 @@ class Adyen_Subscription_Model_Observer extends Mage_Core_Model_Abstract
             $session->setData('subscription_quote_initialized', true);
         }
     }
+
+
+    /**
+     * The BillingAgreement of an subscription can change for IDEAL and Sofort
+     * When you do a recurring transaction for Ideal it will transform the payment to a SEPA payment
+     * This will resolve in a new recurring_detail_reference that you need to use for future payments
+     * so update the subscription with this new reference number
+     * @param Varien_Event_Observer $observer
+     */
+    public function updateBillingAgreementInSubscription(Varien_Event_Observer $observer)
+    {
+        /** @var Mage_Core_Model_Order $order */
+        $order = $observer->getOrder();
+
+        /** @var Varien_OBject $response */
+        $response = $observer->getAdyenResponse();
+
+        $eventCode = trim($response->getData('eventCode'));
+        if($eventCode == Adyen_Payment_Model_Event::ADYEN_EVENT_RECURRING_CONTRACT) {
+
+            // get billingAgreement of the order, an order is always connected to one agreement
+            $billingAgreementId = $this->_getBillingAgreementId($order);
+
+            if ($billingAgreementId) {
+                // check if order has subscription(s)
+
+                // subscription_order
+                $subscriptionOrders = Mage::getModel('adyen_subscription/subscription_order')
+                    ->getCollection()
+                    ->addFieldToFilter('order_id', $order->getId());
+
+                if ($subscriptionOrders->count() <= 0) {
+                    return '';
+                }
+
+                // If the billingagreementId of the subscription does not match the new billingagreementId change the billingAgreementId to this new value
+                foreach($subscriptionOrders as $subscriptionOrders) {
+
+                    $subscription = Mage::getModel('adyen_subscription/subscription')->load($subscriptionOrders->getSubscriptionId());
+                    $billingAgreementIdOfSubs = $subscription->getBillingAgreementId();
+                    if($billingAgreementIdOfSubs != $billingAgreementId) {
+                        try {
+                            $subscription->setBillingAgreementId($billingAgreementId);
+                            $subscription->save();
+                        } catch(Exception $e) {
+                            new Adyen_Subscription_Exception('Could not update subscrription '.$e->getMessage());
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * @param Mage_Sales_Model_Order $order
+     * @return Mage_Sales_Model_Billing_Agreement
+     */
+    protected function _getBillingAgreementId(Mage_Sales_Model_Order $order)
+    {
+        /** @var Mage_Core_Model_Resource $resource */
+        $resource = Mage::getSingleton('core/resource');
+        $connection = $resource->getConnection('core_read');
+        $select = $connection->select();
+
+        $select->from($resource->getTableName('sales/billing_agreement_order'));
+        $select->reset(Zend_Db_Select::COLUMNS);
+        $select->columns('agreement_id');
+        $select->where('order_id = ?', $order->getId());
+        $select->order(array('agreement_id DESC')); // important to get last agreement_id
+
+
+        $billingAgreementId = $connection->fetchOne($select);
+        if (! $billingAgreementId) {
+            Adyen_Subscription_Exception::logException(
+                new Adyen_Subscription_Exception('Could not find billing agreement for order '.$order->getIncrementId())
+            );
+            return null;
+        }
+
+        return $billingAgreementId;
+    }
+
 }
