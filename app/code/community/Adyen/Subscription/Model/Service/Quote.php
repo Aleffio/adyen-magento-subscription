66eb6596b55f1e73c6a722629a852da9c8fb61ac
Paul Hachmang <paul@h-o.nl>
2015-10-09 12:13:35 +0200

    FFM-296: Better commit stategy, solves broken states: $quote->updateSubscription

--- app/code/community/Adyen/Subscription/Model/Service/Quote.php
+++ app/code/community/Adyen/Subscription/Model/Service/Quote.php
@@ -89,32 +89,28 @@ class Adyen_Subscription_Model_Service_Quote
             $subscription->save();
 
             Mage::helper('adyen_subscription')->logOrderCron(sprintf(
-                "Successful created order (#%s) for subscription (#%s)",
+                "Successful created order (%s) for subscription (%s)",
                 $order->getId(), $subscription->getId()
             ));
 
             $order->save();
             $subscription->getResource()->commit();
-
-            Mage::dispatchEvent('adyen_subscription_quote_createorder_after', array(
-                'subscription' => $subscription,
-                'quote' => $quote,
-                'order' => $order
-            ));
-
-            return $order;
         } catch (Mage_Payment_Exception $e) {
+            // 1. rollback everything
             $subscription->getResource()->rollBack();
 
+            // 2. log the error to the debuglog
             Mage::helper('adyen_subscription')->logOrderCron(sprintf(
-                "Error in subscription (#%s) creating order from quote (#%s) error is: %s",
+                "Error in subscription (%s) creating order from quote (%s) error is: %s",
                 $subscription->getId(), $quote->getId(), $e->getMessage()
             ));
 
+            // 3. save the error on the subscription
             $subscription->setStatus($subscription::STATUS_PAYMENT_ERROR);
             $subscription->setErrorMessage($e->getMessage());
             $subscription->save();
 
+            // 4. dispatch the failure event
             Mage::dispatchEvent('adyen_subscription_quote_createorder_fail', array(
                 'subscription' => $subscription,
                 'status' => $subscription->getStatus(),
@@ -122,24 +118,37 @@ class Adyen_Subscription_Model_Service_Quote
             ));
             throw $e;
         } catch (Exception $e) {
+            // 1. rollback everything
             $subscription->getResource()->rollBack();
 
+            // 2. log the error to the debuglog
             Mage::helper('adyen_subscription')->logOrderCron(sprintf(
-                "Error in subscription (#%s) creating order from quote (#%s) error is: %s",
+                "Error in subscription (%s) creating order from quote (%s) error is: %s",
                 $subscription->getId(), $quote->getId(), $e->getMessage()
             ));
 
+            // 3. save the error on the subscription
             $subscription->setStatus($subscription::STATUS_ORDER_ERROR);
             $subscription->setErrorMessage($e->getMessage());
             $subscription->save();
 
+            // 4. dispatch the failure event
             Mage::dispatchEvent('adyen_subscription_quote_createorder_fail',array(
                 'subscription' => $subscription,
+                'quote' => $quote,
                 'status' => $subscription->getStatus(),
                 'error' => $e->getMessage()
             ));
             throw $e;
         }
+
+        Mage::dispatchEvent('adyen_subscription_quote_createorder_after', array(
+            'subscription' => $subscription,
+            'quote' => $quote,
+            'order' => $order
+        ));
+
+        return $order;
     }
 
     /**
@@ -158,117 +167,152 @@ class Adyen_Subscription_Model_Service_Quote
             'quote' => $quote
         ));
 
-        $term = $termType = $stockId = null;
-        foreach ($quote->getItemsCollection() as $quoteItem) {
-            /** @var Mage_Sales_Model_Quote_Item $quoteItem */
-            $productSubscription = $this->_getProductSubscription($quoteItem);
+        try {
+            $subscription->getResource()->beginTransaction();
 
-            if (!$productSubscription) {
-                // No product subscription found, no subscription needs to be created
-                continue;
+            $term = $termType = $stockId = null;
+            foreach ($quote->getItemsCollection() as $quoteItem) {
+                /** @var Mage_Sales_Model_Quote_Item $quoteItem */
+                $productSubscription = $this->_getProductSubscription($quoteItem);
+
+                if (!$productSubscription) {
+                    // No product subscription found, no subscription needs to be created
+                    continue;
+                }
+
+                if (is_null($stockId)) {
+                    $stockId = $quoteItem->getStockId();
+                }
+
+                if (is_null($term)) {
+                    $term = $productSubscription->getTerm();
+                }
+                if (is_null($termType)) {
+                    $termType = $productSubscription->getTermType();
+                }
+                if ($term != $productSubscription->getTerm() || $termType != $productSubscription->getTermType()) {
+                    Adyen_Subscription_Exception::throwException(
+                        'Adyen Subscription options of products in quote have different terms'
+                    );
+                }
             }
 
-            if (is_null($stockId)) {
-                $stockId = $quoteItem->getStockId();
+            $billingAgreement = $this->getBillingAgreement($quote);
+
+            $this->updateQuotePayment($quote, $billingAgreement);
+
+            if (!$quote->getShippingAddress()->getShippingMethod()) {
+                Adyen_Subscription_Exception::throwException('No shipping method selected');
             }
 
-            if (is_null($term)) {
-                $term = $productSubscription->getTerm();
-            }
-            if (is_null($termType)) {
-                $termType = $productSubscription->getTermType();
-            }
-            if ($term != $productSubscription->getTerm() || $termType != $productSubscription->getTermType()) {
-                Adyen_Subscription_Exception::throwException(
-                    'Adyen Subscription options of products in quote have different terms'
-                );
-            }
-        }
-
-        $billingAgreement = $this->getBillingAgreement($quote);
-
-        $this->updateQuotePayment($quote, $billingAgreement);
-
-        if (!$quote->getShippingAddress()->getShippingMethod()) {
-            Adyen_Subscription_Exception::throwException('No shipping method selected');
-        }
-
-        // Update subscription
-        $subscription->setStatus(Adyen_Subscription_Model_Subscription::STATUS_ACTIVE)
-            ->setStockId($stockId)
-            ->setBillingAgreementId($billingAgreement->getId())
-            ->setTerm($term)
-            ->setTermType($termType)
-            ->setShippingMethod($quote->getShippingAddress()->getShippingMethod())
-            ->setUpdatedAt(now())
-            ->save();
-
-        // Create subscription addresses
-        Mage::getModel('adyen_subscription/subscription_address')
-            ->getSubscriptionAddress($subscription, self::ADDRESS_TYPE_BILLING)
-            ->initAddress($subscription, $quote->getBillingAddress())
-            ->save();
-
-        Mage::getModel('adyen_subscription/subscription_address')
-            ->getSubscriptionAddress($subscription, self::ADDRESS_TYPE_SHIPPING)
-            ->initAddress($subscription, $quote->getShippingAddress())
-            ->save();
-
-        // Delete current subscription items
-        foreach ($subscription->getItemCollection() as $subscriptionItem) {
-            /** @var Adyen_Subscription_Model_Subscription_Item $subscriptionItem */
-            $subscriptionItem->delete();
-        }
-
-        $i = 0;
-        // Create new subscription items
-        foreach ($quote->getItemsCollection() as $quoteItem) {
-            /** @var Mage_Sales_Model_Quote_Item $quoteItem */
-
-            /** @var Adyen_Subscription_Model_Product_Subscription $productSubscription */
-            $productSubscription = $this->_getProductSubscription($quoteItem);
-
-            if (!$productSubscription) {
-                // No product subscription found, no subscription needs to be created
-                continue;
-            }
-
-            $productOptions = array(
-                'info_buyRequest' => unserialize($quoteItem->getOptionByCode('info_buyRequest')->getValue()),
-                'additional_options' => unserialize($quoteItem->getOptionByCode('additional_options')->getValue())
-            );
-
-            /** @var Adyen_Subscription_Model_Subscription_Item $subscriptionItem */
-            $subscriptionItem = Mage::getModel('adyen_subscription/subscription_item')
-                ->setSubscriptionId($subscription->getId())
-                ->setStatus(Adyen_Subscription_Model_Subscription_Item::STATUS_ACTIVE)
-                ->setProductId($quoteItem->getProductId())
-                ->setProductOptions(serialize($productOptions))
-                ->setSku($quoteItem->getSku())
-                ->setName($quoteItem->getName())
-                ->setLabel($productSubscription->getLabel())
-                ->setPrice($quoteItem->getPrice())
-                ->setPriceInclTax($quoteItem->getPriceInclTax())
-                ->setQty($quoteItem->getQty())
-                ->setOnce(0)
-                // Currently not in use
-//                ->setMinBillingCycles($productSubscription->getMinBillingCycles())
-//                ->setMaxBillingCycles($productSubscription->getMaxBillingCycles())
-                ->setCreatedAt(now())
+            // Update subscription
+            $subscription->setStatus(Adyen_Subscription_Model_Subscription::STATUS_ACTIVE)
+                ->setStockId($stockId)
+                ->setBillingAgreementId($billingAgreement->getId())
+                ->setTerm($term)
+                ->setTermType($termType)
+                ->setShippingMethod($quote->getShippingAddress()->getShippingMethod())
+                ->setUpdatedAt(now())
                 ->save();
 
-            Mage::dispatchEvent('adyen_subscription_quote_updatesubscription_add_item', array(
-                'subscription' => $subscription,
-                'item' => $subscriptionItem
+            // Create subscription addresses
+            Mage::getModel('adyen_subscription/subscription_address')
+                ->getSubscriptionAddress($subscription, self::ADDRESS_TYPE_BILLING)
+                ->initAddress($subscription, $quote->getBillingAddress())
+                ->save();
+
+            Mage::getModel('adyen_subscription/subscription_address')
+                ->getSubscriptionAddress($subscription, self::ADDRESS_TYPE_SHIPPING)
+                ->initAddress($subscription, $quote->getShippingAddress())
+                ->save();
+
+            // Delete current subscription items
+            foreach ($subscription->getItemCollection() as $subscriptionItem) {
+                /** @var Adyen_Subscription_Model_Subscription_Item $subscriptionItem */
+                $subscriptionItem->delete();
+            }
+
+            $i = 0;
+            // Create new subscription items
+            foreach ($quote->getItemsCollection() as $quoteItem) {
+                /** @var Mage_Sales_Model_Quote_Item $quoteItem */
+
+                /** @var Adyen_Subscription_Model_Product_Subscription $productSubscription */
+                $productSubscription = $this->_getProductSubscription($quoteItem);
+
+                if (!$productSubscription) {
+                    // No product subscription found, no subscription needs to be created
+                    continue;
+                }
+
+                $productOptions = array(
+                    'info_buyRequest' => unserialize($quoteItem->getOptionByCode('info_buyRequest')->getValue()),
+                    'additional_options' => unserialize($quoteItem->getOptionByCode('additional_options')->getValue())
+                );
+
+                /** @var Adyen_Subscription_Model_Subscription_Item $subscriptionItem */
+                $subscriptionItem = Mage::getModel('adyen_subscription/subscription_item')
+                    ->setSubscriptionId($subscription->getId())
+                    ->setStatus(Adyen_Subscription_Model_Subscription_Item::STATUS_ACTIVE)
+                    ->setProductId($quoteItem->getProductId())
+                    ->setProductOptions(serialize($productOptions))
+                    ->setSku($quoteItem->getSku())
+                    ->setName($quoteItem->getName())
+                    ->setLabel($productSubscription->getLabel())
+                    ->setPrice($quoteItem->getPrice())
+                    ->setPriceInclTax($quoteItem->getPriceInclTax())
+                    ->setQty($quoteItem->getQty())
+                    ->setOnce(0)
+                    // Currently not in use
+//                    ->setMinBillingCycles($productSubscription->getMinBillingCycles())
+//                    ->setMaxBillingCycles($productSubscription->getMaxBillingCycles())
+                    ->setCreatedAt(now())
+                    ->save();
+
+                Mage::dispatchEvent('adyen_subscription_quote_updatesubscription_add_item', array(
+                    'subscription' => $subscription,
+                    'item' => $subscriptionItem
+                ));
+
+                $i++;
+            }
+
+            if ($i <= 0) {
+                Adyen_Subscription_Exception::throwException('No subscription products in the subscription');
+            }
+
+            $subscription->getResource()->commit();
+
+            Mage::helper('adyen_subscription')->logOrderCron(sprintf(
+                "Successfully updated subscription (%s) from quote (%s)",
+                $subscription->getId(), $quote->getId()
             ));
 
-            $i++;
+        } catch (Exception $e) {
+            // 1. rollback everything
+            $subscription->getResource()->rollBack();
+
+            // 2. log the error to the debuglog
+            Mage::helper('adyen_subscription')->logOrderCron(sprintf(
+                "Error while updating subscription (%s) from quote (%s) error is: %s",
+                $subscription->getId(), $quote->getId(), $e->getMessage()
+            ));
+
+            // 3. save the error on the subscription
+            $subscription->setErrorMessage($e->getMessage());
+            $subscription->setStatus($subscription::STATUS_SUBSCRIPTION_ERROR);
+            $subscription->save();
+
+            // 4. dispatch the failure event
+            Mage::dispatchEvent('adyen_subscription_quote_updatesubscription_fail',array(
+                'subscription' => $subscription,
+                'quote' => $quote,
+                'status' => $subscription->getStatus(),
+                'error' => $e->getMessage()
+            ));
+            throw $e;
         }
 
-        if ($i <= 0) {
-            Adyen_Subscription_Exception::throwException('No subscription products in the subscription');
-        }
-        
         Mage::dispatchEvent('adyen_subscription_quote_updatesubscription_after', array(
             'subscription' => $subscription,
             'quote' => $quote
