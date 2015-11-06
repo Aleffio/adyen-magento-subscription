6b9ab7f0d51aede396b7b220e9e32e11ceaf1ac6
Maikel Koek <maikel@h-o.nl>
2015-11-06 16:44:12 +0100

    [FFM-358] +: Show billing and shipping address of active quote on scheduled order tab

---
+++ app/code/community/Adyen/Subscription/Block/Adminhtml/Subscription/View/Tabs/Scheduled/Info.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen Subscription module (https://www.adyen.com/)
+ *
+ * Copyright (c) 2015 H&O E-commerce specialists B.V. (http://www.h-o.nl/)
+ * See LICENSE.txt for license details.
+ *
+ * Author: Adyen <magento@adyen.com>, H&O E-commerce specialists B.V. <info@h-o.nl>
+ */
+
+class Adyen_Subscription_Block_Adminhtml_Subscription_View_Tabs_Scheduled_Info
+    extends Mage_Adminhtml_Block_Widget_Grid
+{
+    /**
+     * @return Adyen_Subscription_Model_Subscription
+     */
+    public function getSubscription()
+    {
+        return Mage::registry('adyen_subscription');
+    }
+
+    /**
+     * @return Mage_Sales_Model_Quote
+     */
+    public function getQuote()
+    {
+        return $this->getSubscription()->getActiveQuote();
+    }
+}
