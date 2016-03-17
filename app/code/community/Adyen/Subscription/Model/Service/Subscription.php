307844ff7f5badc06ece8eec45a1e24e669653c6
rikterbeek <rikterbeek@me.com>
2016-03-17 14:04:45 +0100

    #50 revert PR because of issues with configurable products

--- app/code/community/Adyen/Subscription/Model/Service/Subscription.php
+++ app/code/community/Adyen/Subscription/Model/Service/Subscription.php
@@ -66,9 +66,10 @@ class Adyen_Subscription_Model_Service_Subscription
                 $product = Mage::getModel('catalog/product')->load($productId);
                 $product->setData('is_created_from_subscription_item', $subscriptionItem->getId());
 
-                $quoteItem=$quote->addProduct($product, $subscriptionItem->getQty());
-                 //$quoteItem= $quote->getItemByProduct($product); quote fails when adding product. Assign quote item in 
-                 //the line above do the trick,
+                $quote->addProduct($product, $subscriptionItem->getQty());
+
+                // need to use this to retrieve the quoteItem because otherwise configurable products will not work
+                $quoteItem= $quote->getItemByProduct($product);
 
                 if (! $quoteItem instanceof Mage_Sales_Model_Quote_Item) {
                     Mage::helper('adyen_subscription')->logQuoteCron(sprintf('An error occurred while adding a product to the quote: %s', $quoteItem));
