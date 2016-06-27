7db2b30bceb4aa567f91ac8806d9ac9c34717309
Maikel Koek <maikel@h-o.nl>
2016-06-27 18:32:42 +0200

    [FFM-581] +: README.md index file of all readme's, and generation script

--- shell/adyen_subscription.php
+++ shell/adyen_subscription.php
@@ -98,6 +98,56 @@ class Adyen_Subscription_Shell extends Mage_Shell_Abstract
 		$message = Mage::getSingleton('adyen_subscription/cron')->createOrders();
 		echo $message."\n";
 	}
+
+    /**
+     * Generate README.md file as index of all README.md files in module
+     */
+    public function generateReadmeIndexAction()
+    {
+        $baseDir = Mage::getBaseDir();
+        $moduleDir = '/app/code/community/Adyen/Subscription/';
+        $files = $this->rsearch($baseDir . $moduleDir, '/.*README.md/');
+
+        $fileNames = [];
+        foreach ($files as $filename) {
+            $handle = fopen($filename, 'r');
+            $content = fread($handle, filesize($filename));
+            preg_match('# (.+) #', $content, $matches);
+            $fileNames[substr($filename, strlen($baseDir), strlen($filename))] = $matches[0];
+        }
+
+        $readme = fopen($baseDir . $moduleDir . 'README.md', 'w');
+        fwrite($readme, '# Index of README.md files #' . PHP_EOL . PHP_EOL);
+        foreach ($fileNames as $filename => $fileHeader) {
+            if ($moduleDir . 'README.md' == $filename) {
+                continue;
+            }
+
+            $line = '[' . trim($fileHeader) . '](' . trim($filename) . ')' . PHP_EOL;
+
+            fwrite($readme, $line);
+        }
+        fclose($readme);
+
+        echo 'Finished generating README.md index: ' . $moduleDir . 'README.md' . PHP_EOL;
+    }
+
+    /**
+     * @param string $folder
+     * @param string $pattern
+     * @return array
+     */
+    public function rsearch($folder, $pattern)
+    {
+        $dir = new RecursiveDirectoryIterator($folder);
+        $ite = new RecursiveIteratorIterator($dir);
+        $files = new RegexIterator($ite, $pattern, RegexIterator::GET_MATCH);
+        $fileList = array();
+        foreach($files as $file) {
+            $fileList = array_merge($fileList, $file);
+        }
+        return $fileList;
+    }
 }
 
 
